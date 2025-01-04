b0d6943c62ac8f283c00d6af2c6bedf29039b045
mayommi <morehug@nate.com>
2025-01-05 00:52:52 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,120 @@
+$(document).ready(function(){
+  $("#nan2").click(function(){
+    $("#01").fadeIn();
+  });
+  $("#01 button").click(function(){
+    $(".modal").fadeOut();
+  });
+  
+  $("#mang2").click(function(){
+    $("#02").fadeIn();
+  });
+  $("#02 button").click(function(){
+    $(".modal").fadeOut();
+  });
+  
+  $("#eu").click(function(){
+    $("#03").fadeIn();
+  });
+  $("#03 button").click(function(){
+    $(".modal").fadeOut();
+  });
+  
+  $("#lover").click(function(){
+    $("#04").fadeIn();
+  });
+  $("#04 button").click(function(){
+    $(".modal").fadeOut();
+  });
+  
+  $("#jooko").click(function(){
+    $("#05").fadeIn();
+  });
+  $("#05 button").click(function(){
+    $(".modal").fadeOut();
+  });
+
+  $("#heart").click(function(){
+    $("#06").fadeIn();
+  });
+  $("#06 button").click(function(){
+    $(".modal").fadeOut();
+  });
+
+  $("#ong").click(function(){
+    $("#07").fadeIn();
+  });
+  $("#07 button").click(function(){
+    $(".modal").fadeOut();
+  });
+
+  $("#sora").click(function(){
+    $("#08").fadeIn();
+  });
+  $("#08 button").click(function(){
+    $(".modal").fadeOut();
+  });
+
+  $("#dan2").click(function(){
+    $("#09").fadeIn();
+  });
+  $("#09 button").click(function(){
+    $(".modal").fadeOut();
+  });
+
+  $("#huni").click(function(){
+    $("#10").fadeIn();
+  });
+  $("#10 button").click(function(){
+    $(".modal").fadeOut();
+  });
+
+  $("#star").click(function(){
+    $("#11").fadeIn();
+  });
+  $("#11 button").click(function(){
+    $(".modal").fadeOut();
+  });
+
+  $("#woong").click(function(){
+    $("#12").fadeIn();
+  });
+  $("#12 button").click(function(){
+    $(".modal").fadeOut();
+  });
+
+  $("#kuro").click(function(){
+    $("#13").fadeIn();
+  });
+  $("#13 button").click(function(){
+    $(".modal").fadeOut();
+  });
+
+  $("#kyun").click(function(){
+    $("#14").fadeIn();
+  });
+  $("#14 button").click(function(){
+    $(".modal").fadeOut();
+  });
+
+  $("#chae").click(function(){
+    $("#15").fadeIn();
+  });
+  $("#15 button").click(function(){
+    $(".modal").fadeOut();
+  });
+
+  $("#azi").click(function(){
+    $("#16").fadeIn();
+  });
+  $("#16 button").click(function(){
+    $(".modal").fadeOut();
+  });
+
+  $("#semo").click(function(){
+    $("#17").fadeIn();
+  });
+  $("#17 button").click(function(){
+    $(".modal").fadeOut();
+  });
+});
